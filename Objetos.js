22a23515c0571b154f46f349b1f7606c7f88242a
Cristobal Betancurt Palma <cristobal.betancurt2101@alumnos.ubiobio.cl>
2024-09-10 22:50:42 -0300

    ADD Objetos.js

---
+++ Objetos.js
@@ -0,0 +1,28 @@
+//Objetos
+//1. Crear un objeto de persona: Define un objeto llamado "persona" que tenga propiedades como nombre, edad y género. 
+//Luego, muestra la información de esa persona por la consola.
+const persona = {
+    nombre: "Cristobal",
+    edad: 21,
+    genero: "Masculino",
+}
+console.log(persona);
+
+//2. Crear un objeto llamado caja: La caja se compone de cuadernos, lápices, papel, fotografías y indica el estado 
+//si esta en buen estado o no. Imprimir el objeto entero por consola, imprimir cada propiedad del objeto y el tipo de dato que es.
+const caja = {
+    cuadernos: "Auca",
+    lapices: 3,
+    papel: "20 papeles",
+    esta_bueno: true
+}
+console.log(caja);
+console.log(caja.cuadernos);
+console.log(caja.lapices);
+console.log(caja.papel);
+console.log(caja.esta_bueno);
+console.log(typeof caja);
+console.log(typeof caja.cuadernos);
+console.log(typeof caja.lapices);
+console.log(typeof caja.papel);
+console.log(typeof caja.esta_bueno);
